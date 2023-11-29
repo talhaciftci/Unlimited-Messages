cf18d0c35e284a46638368b4bcb451b4b59c6335
talhaciftci <talhaciftci0816@gmail.com>
2023-11-29 18:14:57 +0300

    Tamamlandı

---
+++ pg.py
@@ -0,0 +1,12 @@
+import pyautogui as pg 
+import time
+time.sleep(10)
+#Alttan ne kadar mesaj yollamak istediğimi seç.
+for i in range(100):
+#Alttaki bölümden ne yollamak istediğini seç.
+  pg.write('sa')
+
+
+  pg.press('Enter')
+
+#Buraya sağ tıkla Run in Interactive Window bölümüne gir, kullanacağın bölümü seç (Ben en alttakini kullanıyorum.).bu işlemi yaptığında direkt mesaj atacağın yere fare ile tıkla (farklı bir yer olabilir) unutma 10 saniyen var.=)
